c4436e2a33dc60a654c9d8c304ef1926f8ce01dd
wangj <wangj1106@qq.com>
2018-01-27 16:11:38 +0800

    first commit init project

---
+++ src/main/java/com/bingo/brokendouban/BrokendoubanApplication.java
@@ -0,0 +1,12 @@
+package com.bingo.brokendouban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokendoubanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokendoubanApplication.class, args);
+	}
+}
