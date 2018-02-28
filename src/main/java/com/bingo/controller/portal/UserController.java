5f646a27fcc3ef2bbb19d4605249bc71b9cceb3b
wangj <wangj1106@qq.com>
2018-02-28 09:50:25 +0800

    用户模块

--- src/main/java/com/bingo/controller/portal/UserController.java
+++ src/main/java/com/bingo/controller/portal/UserController.java
@@ -7,6 +7,7 @@ import com.bingo.domain.User;
 import com.bingo.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpSession;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/user/")
 public class UserController {
 
