447f6a657fb54eb14abdda0bbe07a49431cec02b
wangj <wangj1106@qq.com>
2018-03-28 16:17:38 +0800

    修改问卷调查模块

--- src/main/java/com/bingo/controller/portal/UserController.java
+++ src/main/java/com/bingo/controller/portal/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
     @ResponseBody
     public ServerResponse<User> login(String username, String password, HttpSession session) {
         ServerResponse<User> response = iUserService.login(username, password);
-        if (response.isSuccess()) {
+        if (response.getStatus()!=1) {
             session.setAttribute(Const.CURRENT_USER, response.getData());
         }
         return response;
